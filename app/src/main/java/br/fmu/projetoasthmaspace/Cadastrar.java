e4b247bf95bf297bc2ce86925b8938b13655c29e
depaulaed <depauled@hotmail.com>
2025-11-12 16:10:02 -0300

    Atualiza página de Cadastro

--- app/src/main/java/br/fmu/projetoasthmaspace/Cadastrar.java
+++ app/src/main/java/br/fmu/projetoasthmaspace/Cadastrar.java
@@ -1,6 +1,10 @@
 package br.fmu.projetoasthmaspace;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Cadastrar extends AppCompatActivity {
+public class Cadastrar extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cadastrar);
+
+        Spinner spinnerSexo = findViewById(R.id.spinnerSexo);
+        spinnerSexo.setOnItemSelectedListener(this);
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this, adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
